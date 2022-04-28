decce4e3189950df987399c9687e9fd251444e21
Tserewara <alvaro.alf2@gmail.com>
2022-04-28 15:26:54 -0300

    test: ensure add_photo creates photo with correct params

---
+++ tests/domain/usecases/test_add_photo.py
@@ -0,0 +1,16 @@
+from src.domain.models import photo_model
+from src.domain.usecases import add_photo
+
+
+def test_should_add_photo_with_correct_params():
+    image_address = 'any_image_address'
+    user_id = 'any_user_id'
+    result = add_photo(
+        image_address=image_address,
+        user_id=user_id
+    )
+    assert result == photo_model.PhotoModel(
+        image_address=image_address,
+        user_id=user_id
+    )
+
