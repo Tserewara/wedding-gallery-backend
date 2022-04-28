e1332c161f792b683ea29fd72f2ed596b7c0d6cb
Tserewara <alvaro.alf2@gmail.com>
2022-04-28 15:25:27 -0300

    refactor: add __init__.py to improve imports

---
+++ src/domain/models/__init__.py
@@ -0,0 +1,6 @@
+from src.domain.models.photo_model import PhotoModel
+
+__all__ = [
+    'PhotoModel'
+]
+
---
+++ src/domain/usecases/__init__.py
@@ -0,0 +1,7 @@
+from src.domain.usecases.add_photo import add_photo
+
+
+__all__ = [
+    'add_photo'
+]
+
