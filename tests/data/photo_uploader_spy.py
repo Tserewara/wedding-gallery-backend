78a53a6457f429320f279dd8b073c60db8a820c8
Tserewara <alvaro.alf2@gmail.com>
2022-04-28 22:39:05 -0300

    test: ensure add_photo returns photo model if upload succeeds

--- tests/data/photo_uploader_spy.py
+++ tests/data/photo_uploader_spy.py
@@ -1,10 +1,15 @@
+from faker import Faker
 from src.data.protocols import AbstractPhotoUploader
 
 
 class PhotoUploaderSpy(AbstractPhotoUploader):
-    file: str
-    filename: str
+    def __init__(self) -> None:
+        super().__init__()
+        self.file: str = ""
+        self.filename: str = ""
+        self.hash = ""
 
     def upload(self, file, filename):
         self.file = file
         self.filename = filename
+        return f"{self.hash}-{self.filename}"
--- tests/domain/usecases/test_add_photo.py
+++ tests/domain/usecases/test_add_photo.py
@@ -1,6 +1,12 @@
+from faker import Faker
+
+from src.domain.models import PhotoModel
 from src.domain.usecases import add_photo
 from tests.data.photo_repository_spy import PhotoRepositorySpy
 from tests.data.photo_uploader_spy import PhotoUploaderSpy
+
+
+faker = Faker()
 
 
 def test_should_call_photo_uploader_with_correct_params():
@@ -28,3 +34,23 @@ def test_should_add_photo_information_to_repository():
     add_photo(user_id, file, filename, photo_uploader_spy, photo_repository_spy)
 
     assert len(photo_repository_spy.photos) == 1
+
+
+def test_should_return_photo_model_if_upload_succeeds():
+    hash_string = faker.md5()
+    photo_uploader_spy = PhotoUploaderSpy()
+    photo_repository_spy = PhotoRepositorySpy()
+    photo_uploader_spy.hash = hash_string
+
+    user_id = "any_user_id"
+    filename = "any_file_name"
+    image_address = f"{hash_string}-{filename}"
+    file = "any_file"
+
+    photo_model = PhotoModel(user_id, image_address)
+
+    result = add_photo(
+        user_id, file, filename, photo_uploader_spy, photo_repository_spy
+    )
+
+    assert photo_model == result
