f02f5a7f7f1ce261a6aaa1349eb7bb8c8d15f19e
Tserewara <alvaro.alf2@gmail.com>
2022-04-29 04:34:34 -0300

    test: ensure PasswordEncryptor encrypts password when creating user

--- tests/data/usecases/test_create_user.py
+++ tests/data/usecases/test_create_user.py
@@ -1,5 +1,7 @@
 from typing import List
 import pytest
+
+from src.data.protocols import AbstractPasswordEncryptor
 from src.domain.errors import EmailInUseError
 from src.domain.models import UserModel
 from src.data.usecases import CreateUser
@@ -19,13 +21,27 @@ class UserRepositorySpy(AbstractUserRepository):
                 return user
 
 
+class PasswordEncryptorSpy(AbstractPasswordEncryptor):
+    def __init__(self) -> None:
+        self.hash = None
+        self.encrypted_password = ""
+
+    def encrypt_password(self, password) -> str:
+        self.encrypted_password = f"{self.hash}-{password}"
+        return self.encrypted_password
+
+    def check_password(self, password) -> bool:
+        return password == self.encrypted_password.strip(f"{self.hash}-")
+
+
 def test_should_add_user_created_to_repository():
     name = "a_username"
     email = "random@example.com"
     password = "Secure_Password"
     is_admin = True
     user_repository_spy = UserRepositorySpy()
-    sut = CreateUser(user_repository_spy)
+    password_encryptor = PasswordEncryptorSpy()
+    sut = CreateUser(user_repository_spy, password_encryptor)
     sut.create(name, email, password, is_admin)
 
     assert len(user_repository_spy.users) == 1
@@ -37,8 +53,26 @@ def test_should_raise_email_in_user_error_if_email_already_exists():
     password = "Secure_Password"
     is_admin = True
     user_repository_spy = UserRepositorySpy()
-    sut = CreateUser(user_repository_spy)
+    password_encryptor = PasswordEncryptorSpy()
+    sut = CreateUser(user_repository_spy, password_encryptor)
     sut.create(name, email, password, is_admin)
 
     with pytest.raises(EmailInUseError):
         sut.create(name, email, password, is_admin)
+
+
+def test_encrypt_password_when_creating_user():
+    name = "a_username"
+    email = "random@example.com"
+    password = "Secure@Password"
+    is_admin = True
+    user_repository_spy = UserRepositorySpy()
+    password_encryptor = PasswordEncryptorSpy()
+    fake_hash = "78asd"
+    password_encryptor.hash = fake_hash
+
+    sut = CreateUser(user_repository_spy, password_encryptor)
+
+    sut.create(name, email, password, is_admin)
+
+    assert user_repository_spy.users[0].password == f"{fake_hash}-{password}"
