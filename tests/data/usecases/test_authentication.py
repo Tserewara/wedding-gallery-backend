5152228a85a4e11f2e3db455e6731f20ec69a9b9
Tserewara <alvaro.alf2@gmail.com>
2022-04-29 15:45:18 -0300

    test: ensure Authentication raises WrongCredentialsError if email is not found

--- tests/data/usecases/test_authentication.py
+++ tests/data/usecases/test_authentication.py
@@ -1,23 +1,12 @@
-from src.data.protocols import AbstractUserRepository, AbstractPasswordEncryptor
-from src.domain.models.user_model import UserModel
-from src.domain.usecases.abstract_authentication import AbstractAuthentication
+import pytest
+
+from src.data.usecases import Authentication
+
+from src.domain.errors import WrongCredentialsError
+from src.domain.models import UserModel
 
 from tests.data.mocks.user_repository_spy import UserRepositorySpy
 from tests.data.mocks.password_encryptor_spy import PasswordEncryptorSpy
-
-
-class Authentication(AbstractAuthentication):
-    def __init__(
-        self,
-        user_repository: AbstractUserRepository,
-        password_encryptor: AbstractPasswordEncryptor,
-    ) -> None:
-        super().__init__(user_repository, password_encryptor)
-
-    def auth(self, email, password):
-        user: UserModel = self.user_repository.find_user_by_email(email)
-        print(user.password)
-        return self.password_encryptor.check_password(password, user.password)
 
 
 def test_should_return_true_if_authentication_succeeds():
@@ -33,3 +22,17 @@ def test_should_return_true_if_authentication_succeeds():
     sut = Authentication(user_repository_spy, password_encryptor_spy)
     result = sut.auth(email, password)
     assert result == True
+
+
+def test_should_raise_wrong_credentials_error_if_email_is_not_found():
+    user_repository_spy = UserRepositorySpy()
+    password_encryptor_spy = PasswordEncryptorSpy()
+    password_encryptor_spy.hash = "14a5s6"
+
+    email = "user@example.com"
+    password = password_encryptor_spy.encrypt_password("any_password")
+
+    sut = Authentication(user_repository_spy, password_encryptor_spy)
+
+    with pytest.raises(WrongCredentialsError):
+        sut.auth(email, password)
