cf807490213b2f229df9f06f9ee8b516813cd745
Tserewara <alvaro.alf2@gmail.com>
2022-04-30 20:10:04 -0300

    feat: ensure AddComment raises MissingParamError if text is None

--- src/data/usecases/add_comment.py
+++ src/data/usecases/add_comment.py
@@ -1,3 +1,4 @@
+from src.domain.errors.missing_param_error import MissingParamError
 from src.data.protocols import AbstractPhotoRepository
 from src.domain.usecases import AbstractAddComment
 
@@ -7,4 +8,6 @@ class AddComment(AbstractAddComment):
         super().__init__(photo_repository)
 
     def comment(self, user_id, photo_id, text):
+        if text is None:
+            raise MissingParamError("You must add some content")
         self.photo_repository.update_photo(photo_id, "comments", {user_id: text})
