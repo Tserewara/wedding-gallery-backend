46e5cf5eb7eec4f7a9ebae19e0c004764ecefbab
Tserewara <alvaro.alf2@gmail.com>
2022-04-30 17:47:26 -0300

    feat: ensure ApprovePhotoController returns 404 if user or photo are not found

--- src/presentation/controllers/approve_photo_controller.py
+++ src/presentation/controllers/approve_photo_controller.py
@@ -1,6 +1,8 @@
 from flask import jsonify, request
 from flask.views import MethodView
 
+from src.domain.errors.photo_not_found_error import PhotoNotFoundError
+from src.domain.errors.user_not_found_error import UserNotFoundError
 from src.main.factories.infra.mongo_photo_repository_factory import (
     mongo_photo_repository_factory,
 )
@@ -19,8 +21,14 @@ approve_photo = ApprovePhoto(
 class ApprovePhotoController(MethodView):
     def patch(self):
 
-        approve_photo.approve(
-            user_id=request.json.get("user_id"), photo_id=request.json.get("photo_id")
-        )
+        try:
 
-        return jsonify(msg="photo approved"), 200
+            approve_photo.approve(
+                user_id=request.json.get("user_id"),
+                photo_id=request.json.get("photo_id"),
+            )
+
+            return jsonify(msg="photo approved"), 200
+
+        except (UserNotFoundError, PhotoNotFoundError) as e:
+            return jsonify(msg=str(e)), 404
