972b9640641b6159f29a9740b50ff4db778cbfb8
Tserewara <alvaro.alf2@gmail.com>
2022-04-30 14:50:52 -0300

    feat: ensure AuthenticationController returns an access token if authentication succeeds

---
+++ src/presentation/controllers/authentication_controller.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+from flask import jsonify, request
+from flask.views import MethodView
+from flask_jwt_extended import (
+    create_access_token,
+)
+
+from src.data.usecases import Authentication
+from src.main.factories.infra import (
+    mongo_user_repository_factory,
+    passlib_password_encryptor_factory,
+)
+
+authentication = Authentication(
+    mongo_user_repository_factory(), passlib_password_encryptor_factory()
+)
+
+
+class AuthenticationController(MethodView):
+    def post(self):
+
+        result, user_id = authentication.auth(
+            email=request.json.get("email"), password=request.json.get("password")
+        )
+
+        if result:
+
+            access_token = create_access_token(
+                identity=str(user_id), expires_delta=timedelta(minutes=30)
+            )
+
+            return jsonify({"token": access_token, "user_id": user_id}), 200
--- src/presentation/create_app.py
+++ src/presentation/create_app.py
@@ -1,12 +1,24 @@
+import os
 from flask import Flask
+from flask_jwt_extended import JWTManager
+
+from src.presentation.controllers.authentication_controller import (
+    AuthenticationController,
+)
 from src.presentation.controllers.users_controller import UsersController
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+    JWTManager(app)
 
     app.add_url_rule(
         "/users", methods=["POST"], view_func=UsersController.as_view("users")
+    )
+
+    app.add_url_rule(
+        "/login", methods=["POST"], view_func=AuthenticationController.as_view("login")
     )
 
     return app
