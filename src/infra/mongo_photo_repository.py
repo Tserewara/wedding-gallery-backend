492d5cbd321d6def7daae555da0e53b6acbdce5f
Tserewara <alvaro.alf2@gmail.com>
2022-04-29 22:33:18 -0300

    feat: add find_photo_by_id and update_photo to MongoPhotoRepository

--- src/infra/mongo_photo_repository.py
+++ src/infra/mongo_photo_repository.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from pymongo.collection import Collection
 from pymongo.database import Database
 from src.domain.models import PhotoModel
@@ -15,4 +16,14 @@ class MongoPhotoRepository(AbstractPhotoRepository):
     def add(self, photo: PhotoModel) -> None:
         self._collection.insert_one(
             {"image_address": photo.image_address, "user_id": photo.user_id}
+        )
+
+    def find_photo_by_id(self, photo_id: str) -> PhotoModel:
+        return self._collection.find_one({"_id": ObjectId(photo_id)})
+
+    def update_photo(self, photo_id: str, fieldname: str, value=...):
+        action = {"is_approved": "$set", "likes": "$push", "comments": "$push"}
+
+        self._collection.update_one(
+            {"_id": ObjectId(photo_id)}, {action[fieldname]: {fieldname: value}}
         )
