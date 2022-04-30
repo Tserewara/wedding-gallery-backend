1b1e5bf56a50a28bc455d5c12fa0fe56112dd9d4
Tserewara <alvaro.alf2@gmail.com>
2022-04-30 20:10:14 -0300

    test: ensure AddComment raises MissingParamError if text is None

--- tests/data/usecases/test_add_comment.py
+++ tests/data/usecases/test_add_comment.py
@@ -1,5 +1,19 @@
+import pytest
+
 from src.data.usecases import AddComment
+from src.domain.errors import MissingParamError
 from tests.data.mocks.photo_repository_spy import PhotoRepositorySpy
+
+
+def test_should_raise_missing_param_error_if_text_is_not_passed():
+    photo_repository_spy = PhotoRepositorySpy()
+    sut = AddComment(photo_repository_spy)
+
+    user_id = "0"
+    photo_id = "0"
+
+    with pytest.raises(MissingParamError):
+        sut.comment(user_id, photo_id, None)
 
 
 def test_should_like_photo():
