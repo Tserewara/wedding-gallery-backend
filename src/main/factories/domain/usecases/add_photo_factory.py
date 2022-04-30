62f937ef14f60d8fc9cec762470cb06ee051c0f7
Tserewara <alvaro.alf2@gmail.com>
2022-04-30 17:17:21 -0300

    refactor: change add_photo_factory to use other factories

--- src/main/factories/domain/usecases/add_photo_factory.py
+++ src/main/factories/domain/usecases/add_photo_factory.py
@@ -1,10 +1,9 @@
-from src.infra.s3.s3_photo_uploader import S3PhotoUploader
+from src.main.factories.infra.s3_photo_uploader_factory import s3_photo_uploader_factory
+from src.main.factories.infra.mongo_photo_repository_factory import (
+    mongo_photo_repository_factory,
+)
 from src.data.usecases import AddPhoto
-from src.main.factories.infra.mongo_user_repository_factory import mongo_client_factory
-from src.infra.mongo.mongo_photo_repository import MongoPhotoRepository
 
 
 def add_photo_factory():
-    photo_uploader = S3PhotoUploader()
-    photo_repository = MongoPhotoRepository(*mongo_client_factory(), "photos")
-    return AddPhoto(photo_uploader, photo_repository)
+    return AddPhoto(s3_photo_uploader_factory(), mongo_photo_repository_factory())
